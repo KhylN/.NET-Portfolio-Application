e6c5c167283aa2f0e9e28a58eda41b1d7eb186f2
Khyl Nad <khylanad@gmail.com>
2025-08-17 21:33:42 -0600

    Add Microsoft.AspNetCore.Components.Authorization package and configure authentication services

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,21 @@
+using SkillSnap.Client.Services;     // for ApiClient
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using FINAL_PROJECT.Client;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using SkillSnap.Client.Services.Auth; // for TokenAuthStateProvider
+using FINAL_PROJECT.Client; // for App
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<TokenAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<TokenAuthStateProvider>());
+builder.Services.AddScoped<ApiClient>();
 
 await builder.Build().RunAsync();
